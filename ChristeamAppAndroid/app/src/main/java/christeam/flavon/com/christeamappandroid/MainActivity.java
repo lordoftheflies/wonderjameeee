53f049074fbad45af63334ec3653fac3984c8785
lordoftheflies <heglas11@gmail.com>
2016-04-12 21:56:15 +0200

    Create container without titlle. Remove margins and padding from the content.

--- ChristeamAppAndroid/app/src/main/java/christeam/flavon/com/christeamappandroid/MainActivity.java
+++ ChristeamAppAndroid/app/src/main/java/christeam/flavon/com/christeamappandroid/MainActivity.java
@@ -2,12 +2,40 @@ package christeam.flavon.com.christeamappandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        myWebView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        myWebView.setWebChromeClient(new WebChromeClient());
+        //webSettings.setAllowContentAccess(true);
+        //webSettings.setAllowFileAccess(true);
+        //webSettings.setDomStorageEnabled(true);
+        //webSettings.setAllowUniversalAccessFromFileURLs(true);
+        //webSettings.setLoadWithOverviewMode(false);
+        //webSettings.setUseWideViewPort(true);
+        //webSettings.setBuiltInZoomControls(true);
+        //webSettings.setUserAgentString("christeam-agent");
+
+        myWebView.loadUrl("http://192.168.1.103:5000/");
     }
 }
