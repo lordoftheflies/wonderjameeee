55b123f5a9640a25a2c1630c48a47368b605cb9a
lordoftheflies <heglas11@gmail.com>
2016-04-12 22:09:57 +0200

    Configurable base url for the web app.

--- ChristeamAppAndroid/app/src/main/java/christeam/flavon/com/christeamappandroid/MainActivity.java
+++ ChristeamAppAndroid/app/src/main/java/christeam/flavon/com/christeamappandroid/MainActivity.java
@@ -14,28 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-
         myWebView = (WebView) findViewById(R.id.webView);
-
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDatabaseEnabled(true);
         webSettings.setDomStorageEnabled(true);
         myWebView.setWebChromeClient(new WebChromeClient());
-        //webSettings.setAllowContentAccess(true);
-        //webSettings.setAllowFileAccess(true);
-        //webSettings.setDomStorageEnabled(true);
-        //webSettings.setAllowUniversalAccessFromFileURLs(true);
-        //webSettings.setLoadWithOverviewMode(false);
-        //webSettings.setUseWideViewPort(true);
-        //webSettings.setBuiltInZoomControls(true);
-        //webSettings.setUserAgentString("christeam-agent");
-
-        myWebView.loadUrl("http://192.168.1.103:5000/");
+        myWebView.loadUrl(getResources().getString(R.string.base_url));
     }
 }
