04b736cb0e43f5f7a900398c4b8b75f5575a7639
lordoftheflies <heglas11@gmail.com>
2016-04-12 22:15:09 +0200

    Documentations. Fixes #13

--- ChristeamAppAndroid/app/src/main/java/christeam/flavon/com/christeamappandroid/MainActivity.java
+++ ChristeamAppAndroid/app/src/main/java/christeam/flavon/com/christeamappandroid/MainActivity.java
@@ -15,11 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Reference to web-view.
         myWebView = (WebView) findViewById(R.id.webView);
+        // Setup for use Polymer
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDatabaseEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        // Locate base url of the frontend from the webapp.
         myWebView.setWebChromeClient(new WebChromeClient());
         myWebView.loadUrl(getResources().getString(R.string.base_url));
     }
